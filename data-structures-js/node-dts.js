b862207b6fadda5d4d8d8ea5de451cc1787dafb4
midoostar1 <95588511+midoostar1@users.noreply.github.com>
2023-08-21 13:43:24 -0500

    seting and updating the pointer of next node

--- data-structures-js/node-dts.js
+++ data-structures-js/node-dts.js
@@ -1,12 +1,35 @@
-class Node{
-    constructor(data){
-        this.data = data;
-        this.next = null;
-}
+//creating a Node class
+class Node {
+  //constructor
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 
-setNextNode(node){
-if(node instanceof Node || node === null){
-    this.next = node;
-}else{
-    throw new Error("Node must be a Node")
+  // next setter
+  setNextNode(node) {
+    if (node instanceof Node || node === null) {
+      this.next = node;
+    } else {
+      throw new Error("node must be a type Node");
+    }
+  }
+
+  //next getter
+  getNextNode() {
+    return this.next;
+  }
 }
+//creating instances of Node class
+const firstNode = new Node("I am firstNode");
+const secondNode = new Node("I am secondNode");
+const thirdNode = new Node("I am thirdNode");
+//error (next is not of type Node)
+//firstNode.setNextNode("next");
+
+firstNode.setNextNode(secondNode);
+console.log(firstNode.getNextNode());
+
+firstNode.setNextNode(thirdNode)
+console.log(firstNode.getNextNode());
+
